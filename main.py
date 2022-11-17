d269ac1a0a394776f2977ae1595bc558be22a4f3
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-11-18 00:34:42 +0100

    save received data as a plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from matplotlib import pyplot as plt
 
 
 app = Flask(__name__)
@@ -12,7 +13,15 @@ def check_server():
 @app.route("/recording", methods=["POST"])
 def new_recording():
     data = request.json
-    print(data)
+    x_axis_values = [float(x) for x in data["x"]]
+    y_axis_values = [float(y) for y in data["y"]]
+    z_axis_values = [float(z) for z in data["z"]]
+    sample_no = list(range(len(x_axis_values)))
+    fig, ax = plt.subplots()
+    ax.plot(sample_no, x_axis_values)
+    ax.plot(sample_no, y_axis_values)
+    ax.plot(sample_no, z_axis_values)
+    fig.savefig("./last_movement.png")
     return "OK"
 
 
