fbea1d003325ea6c352d6d97552881a4f6197579
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-11-27 17:52:44 +0100

    allowed saving received data to appropriate directories

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
 from matplotlib import pyplot as plt
+import os
+import pathlib
+import json
+import time
 
+
+DATASET_DIR = "dataset"
 
 app = Flask(__name__)
 
@@ -10,20 +16,29 @@ def check_server():
     return "OK"
 
 
-@app.route("/recording", methods=["POST"])
+@app.route("/new", methods=["POST"])
 def new_recording():
     data = request.json
-    x_axis_values = [float(x) for x in data["x"]]
-    y_axis_values = [float(y) for y in data["y"]]
-    z_axis_values = [float(z) for z in data["z"]]
-    sample_no = list(range(len(x_axis_values)))
-    fig, ax = plt.subplots()
-    ax.plot(sample_no, x_axis_values)
-    ax.plot(sample_no, y_axis_values)
-    ax.plot(sample_no, z_axis_values)
-    fig.savefig("./last_movement.png")
+
+    motion_class = data["class"]
+
+    class_dir = os.path.join(DATASET_DIR, motion_class)
+    os.makedirs(class_dir, exist_ok=True)
+
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    motion_data_file = f"{motion_class}_{timestamp}.json"
+    path_to_save = os.path.join(class_dir, motion_data_file)
+
+    with open(path_to_save, "w") as file:
+        json.dump(data, file, indent=4)
+    
     return "OK"
 
 
 if __name__ == "__main__":
+
+    if os.path.exists(DATASET_DIR):
+        if (pathlib.Path(DATASET_DIR)).is_file():
+            raise Exception("Dataset dir is a file")
+        
     app.run(host="0.0.0.0", port=8080)
