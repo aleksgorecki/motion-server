8f75e6a35c66a1c8f83425b79ed22cd0e805e56a
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-12-17 17:12:46 +0100

    added rolling average filtering and horizontal shifting

--- data_processing.py
+++ data_processing.py
@@ -5,114 +5,12 @@ import os
 from matplotlib import pyplot as plt
 import json
 from numpy.typing import NDArray, ArrayLike
-
-
-class Motion:
-    samples: NDArray  # samples[1][len_of_motion][3]
-
-    def __init__(self, x: ArrayLike, y: ArrayLike, z: ArrayLike, three_channel_array: NDArray = None) -> None:
-        if three_channel_array is None:
-            self.samples = np.empty(shape=(1, len(x), 3))
-            for sample in range(len(x)):
-                self.samples[0][sample][0] = x[sample]
-                self.samples[0][sample][1] = y[sample]
-                self.samples[0][sample][2] = z[sample]
-        else:
-            self.samples = three_channel_array
-
-    @staticmethod
-    def from_json(json_object) -> Motion:
-        return Motion(
-            x=json_object['x'],
-            y=json_object['y'],
-            z=json_object['z']
-        )
-
-    @staticmethod
-    def from_array(array: NDArray) -> Motion:
-        return Motion(
-            three_channel_array=array
-        )
-
-    def __len__(self):
-        return self.samples.shape[1]
-
-    def get_separated_axis(self, axis_idx: int):
-        axis_arr = np.empty(shape=(len(self)))
-        for idx, sample in enumerate(self.samples[0]):
-            axis_arr[idx] = sample[axis_idx]
-        return axis_arr
-
-    def get_x(self) -> NDArray:
-        return self.get_separated_axis(0)
-
-    def get_y(self) -> NDArray:
-        return self.get_separated_axis(1)
-
-    def get_z(self) -> NDArray:
-        return self.get_separated_axis(2)
-
-    def get_global_extremum_position(self) -> int:
-
-        max_pos = np.unravel_index(self.samples.argmax(), shape=self.samples.shape)
-        max_val = self.samples[max_pos]
-        min_pos =  np.unravel_index(self.samples.argmin(), shape=self.samples.shape)
-        min_val = self.samples[min_pos]
-
-        if max_val > abs(min_val):
-            return max_pos[1]
-        else:
-            return min_pos[1]
-
-    def crop(self, center_position: int, half_span: int) -> None:
-        lower_bound = center_position - half_span
-        lower_padding = 0
-        if lower_bound < 0:
-            lower_padding = abs(lower_bound)
-            lower_bound = 0
-
-        full_axis_len = self.samples.shape[1]
-        upper_bound = center_position + half_span
-        upper_padding = 0
-        if upper_bound > full_axis_len:
-            upper_padding = upper_bound - full_axis_len
-            upper_bound = full_axis_len
-
-        cropped_arr = np.zeros(shape=(1, 2 * half_span, 3))
-        cropped_arr[0][0 + lower_padding:2 * half_span - upper_padding][:] = self.samples[0][lower_bound:upper_bound][:]
-        self.samples = cropped_arr
-
-    def scale_values(self, factor: int) -> None:
-        self.samples = self.samples * factor
-
-    # def filter_high_frequencies(self) -> None:
-    #     ones_len = int(len(self.x) / 10)
-    #     self.x = np.convolve(self.x, np.ones(ones_len), 'same') / ones_len
-    #     self.y = np.convolve(self.y, np.ones(ones_len), 'same') / ones_len
-    #     self.z = np.convolve(self.z, np.ones(ones_len), 'same') / ones_len
-
-    def get_array(self) -> NDArray:
-        return np.array(self.samples)
-
-    def save_as_bitmap(self, savefile: str) -> None:
-        im_array = np.array(self.samples)
-
-        im_min = min(im_array)
-        im_max = max(im_array)
-
-        scaled_arr = ((im_array - im_min) / (im_max - im_min)) * 255
-        scaled_arr = np.array(scaled_arr, dtype=np.uint8)
-
-        im = Image.fromarray(scaled_arr, mode="RGB")
-        im.save(savefile, format="BMP")
+from motion import Motion
 
 
 def plot_motion(motion: Motion, savefile: str):
     fig, ax = plt.subplots()
     axis_names = ["x", "y", "z"]
-    x = motion.get_x()
-    y = motion.get_y()
-    z = motion.get_z()
     for idx, motion_axis in enumerate((motion.get_x(), motion.get_y(), motion.get_z())):
         ax.plot(range(len(motion)), motion_axis, label=axis_names[idx])
     ax.legend()
@@ -142,5 +40,9 @@ if __name__ == "__main__":
     with open("./dataset/zneg/zneg_20221204_182105.json", "r") as json_file_handle:
         json_object = json.load(json_file_handle)
     data: Motion = Motion.from_json(json_object)
+    x = data.samples[0][0][:]
     data.crop(data.get_global_extremum_position(), 40)
     plot_motion(data, "test_plot.jpg")
+    data.horizontal_shift(-0.2)
+    data.average_filter(5)
+    plot_motion(data, "test_plot_shifted.jpg")
---
+++ motion.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+import numpy as np
+from PIL import Image
+from numpy.typing import NDArray, ArrayLike
+
+
+class Motion:
+    samples: NDArray  # samples[1][len_of_motion][3]
+
+    def __init__(self, three_channel_array: NDArray) -> None:
+        self.samples = three_channel_array
+
+    @staticmethod
+    def from_json(json_object) -> Motion:
+        x = json_object['x']
+        z = json_object['y']
+        y = json_object['z']
+        three_channel_array = np.empty(shape=(1, len(x), 3))
+        for sample in range(len(x)):
+            three_channel_array[0][sample][0] = x[sample]
+            three_channel_array[0][sample][1] = y[sample]
+            three_channel_array[0][sample][2] = z[sample]
+        return Motion(
+            three_channel_array
+        )
+
+    def get_copy(self) -> Motion:
+        return Motion(np.array(self.samples))
+
+    def __len__(self):
+        return self.samples.shape[1]
+
+    def get_x(self) -> NDArray:
+        return self.samples[0][:,0]
+
+    def get_y(self) -> NDArray:
+        return self.samples[0][:,1]
+
+    def get_z(self) -> NDArray:
+        return self.samples[0][:,2]
+
+    def get_global_extremum_position(self) -> int:
+
+        max_pos = np.unravel_index(self.samples.argmax(), shape=self.samples.shape)
+        max_val = self.samples[max_pos]
+        min_pos =  np.unravel_index(self.samples.argmin(), shape=self.samples.shape)
+        min_val = self.samples[min_pos]
+
+        if max_val > abs(min_val):
+            return max_pos[1]
+        else:
+            return min_pos[1]
+
+    def crop(self, center_position: int, half_span: int) -> None:
+        lower_bound = center_position - half_span
+        lower_padding = 0
+        if lower_bound < 0:
+            lower_padding = abs(lower_bound)
+            lower_bound = 0
+
+        full_axis_len = self.samples.shape[1]
+        upper_bound = center_position + half_span
+        upper_padding = 0
+        if upper_bound > full_axis_len:
+            upper_padding = upper_bound - full_axis_len
+            upper_bound = full_axis_len
+
+        cropped_arr = np.zeros(shape=(1, 2 * half_span, 3))
+        cropped_arr[0][0 + lower_padding:2 * half_span - upper_padding][:] = self.samples[0][lower_bound:upper_bound][:]
+        self.samples = cropped_arr
+
+    def scale_values(self, factor: int) -> None:
+        self.samples = self.samples * factor
+
+    def horizontal_shift(self, ratio: float):
+        new_arr = np.zeros(shape=self.samples.shape)
+        threshold_idx = abs(int(ratio * len(self)))
+        if ratio < 0:
+            new_arr[0][0:len(self) - threshold_idx] = self.samples[0][threshold_idx:]
+        else:
+            new_arr[0][threshold_idx:] = self.samples[0][0:len(self) - threshold_idx]
+        self.samples = new_arr
+
+    def average_filter(self, kernel_size: int = 5):
+        axis_columns = (self.get_x(), self.get_y(), self.get_z())
+        filtered_columns = []
+        kernel = np.ones(kernel_size) / kernel_size
+        for column in axis_columns:
+            filtered_columns.append(np.convolve(column, kernel, mode="same"))
+
+        self.samples = np.array([np.column_stack(filtered_columns)])
+
+
+    def get_array(self) -> NDArray:
+        return np.array(self.samples)
+
+    def save_as_bitmap(self, savefile: str) -> None:
+        im_array = np.array(self.samples)
+
+        im_min = min(im_array)
+        im_max = max(im_array)
+
+        scaled_arr = ((im_array - im_min) / (im_max - im_min)) * 255
+        scaled_arr = np.array(scaled_arr, dtype=np.uint8)
+
+        im = Image.fromarray(scaled_arr, mode="RGB")
+        im.save(savefile, format="BMP")
